c970332ad29410531b4595d3ea17bd291528d9b2
Vishakha Atkare <vishakha.atkare@mintree.com>
2022-09-24 14:20:22 +0530

    adding project

--- CA backend in Spring Boot/doctor/src/test/java/com/example/doctor/DoctorApplicationTests.java
+++ CA backend in Spring Boot/doctor/src/test/java/com/example/doctor/DoctorApplicationTests.java
@@ -3,11 +3,29 @@ package com.example.doctor;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import com.example.doctor.entity.Doctor;
+import com.example.doctor.repository.DoctorRepository;
+
 @SpringBootTest
 class DoctorApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void contextLoads() { 
+		
 	}
+		@Test
+		public void test_AddDoctor() {
+			Doctor  doctorEntities = new Doctor();
+			doctorEntities.setId(6);
+			doctorEntities.setName("Gauri");
+			doctorEntities.setGender("Female");
+			doctorEntities.setSpecialist("Child");
+			doctorEntities.setNumberOfPatients(12);
+			doctorEntities.setAge(22);
+		
+	}
+	
+	
+	
 
 }
--- CA backend in Spring Boot/patient/src/test/java/com/example/patient/PatientApplicationTests.java
+++ CA backend in Spring Boot/patient/src/test/java/com/example/patient/PatientApplicationTests.java
@@ -3,11 +3,23 @@ package com.example.patient;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import com.example.patient.entity.Patient;
+
 @SpringBootTest
 class PatientApplicationTests {
 
 	@Test
 	void contextLoads() {
 	}
+	@Test
+	public void test_AddDoctor() {
+		Patient  doctorEntities = new Patient();
+		doctorEntities.setId(2);
+		doctorEntities.setName("Sanjay");
+		doctorEntities.setVisitedDoctor("Gauri");
+		doctorEntities.setDateOfVisit("2006-02-21");
+		doctorEntities.setPrescription("Comman Cold");
+	}
+	
 
 }
